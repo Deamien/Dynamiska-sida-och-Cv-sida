8eb156abfbcbf280fd69faeeb7a2033787693abf
Deamien <snejko96@gmail.com>
2024-06-18 20:35:15 +0200

    fixed X button, should be consistent with the popup box

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,12 +6,12 @@ const Modal = ({ isOpen, onClose }) => {
   return (
     <div className="modal" style={{ display: isOpen ? 'block' : 'none' }}>
         <div className='modal-content'>
+          <span className="close" onClick={onClose}>&times;</span>
           <p>Why did the two Java methods get a divorce?</p>
           <SpoilerText>
             <p>Because they had constant arguments.</p>
           </SpoilerText>
         </div>
-      <span className="close" onClick={onClose}>&times;</span>
     </div>
   );
 };
